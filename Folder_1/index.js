30d2348d42d7c142f0af5f95f047f73c28020270
DmitryShastel <dmshastel@gmail.com>
2022-07-23 18:08:15 +0300

    F.lesson_JS_Operators task_7 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,12 +1,16 @@
 let a = +prompt("Fill in your age", 0);
 
-if (a >= 14 && a <= 90) {
-    alert('Your Welcome man');
-} else if (a >= 0 && a <= 13) {
+if (!(a >= 14 && a <= 90)) {
     alert('You are kid, get lose');
-} else if (a >= 90 && a <= 130) {
+} else if (a !== 14 && a !== 90) {
+    alert('Your Welcome man');
+} else if (a > 90 && a <= 130) {
     alert('You are old timer, get lose');
-}
+    }
+
+
+
+
 
 
 
