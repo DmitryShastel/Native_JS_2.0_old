be1cc8289323dee3daeb8eb119d539614f66527a
DmitryShastel <dmshastel@gmail.com>
2022-07-25 17:29:09 +0300

    F.lesson_JS_Operators task_10 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,17 +1,11 @@
-let a = 2 * (3 - 1);
-let b = 6 - 2;
+let x = 7;
+let y = null;
+let z = 9;
 
-console.log(a == b);
 
-let c = 5 * (7 - 4);
-let d = 1 + 2 + 7;
+console.log(z || x && y);
 
-console.log(c > d);
 
-let e = 2 ** 4;
-let f = 4 ** 2; 
-
-console.log(e != f);
 
 
 
