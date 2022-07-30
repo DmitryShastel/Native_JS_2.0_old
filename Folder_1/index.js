fa4866dfbee209fbd3fb296d0ee3f0f30e70c6e5
DmitryShastel <dmshastel@gmail.com>
2022-07-30 20:06:04 +0300

    F.lesson_JS_Operators task_35 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,9 +1,9 @@
-let num = 10;
-// let num1 = num++;
-let num2 = ++num;
-// let num3 = num--;
 
-// console.log(num1);
-console.log(num2);
-// console.log(num3);
-console.log(num);
+let h = 5;
+
+let str1 = h;
+let str2 = h ** 2;
+
+
+console.log('h = ' + str1);
+console.log('h in the square = ' + str2);
