245c405da70524320b42430a0a8ab63eda98c3de
DmitryShastel <dmshastel@gmail.com>
2022-07-31 14:21:53 +0300

    F.lesson_JS_Operators task_37 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,9 +1,5 @@
-let a = 5;
-let b = 5;
-
-let str1 = a * b;
-let str2 = a + b;
-
-
-console.log('The multiplication of numbers a and b is ' + str1);
-console.log('he sum of numbers a and b is ' + str2);
+    let a = +prompt('Enter first number');
+    let b = +prompt('Enter second number');
+    console.log(a ** 2);
+    console.log(b ** 2);
+    console.log((a ** 2)+(b ** 2));
