3aaf40f07ca5aff0f333ad6dfc7fde8ecd24f8aa
DmitryShastel <dmshastel@gmail.com>
2022-07-31 16:24:18 +0300

    F.lesson_JS_Operators task_42 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,9 +1,19 @@
-    let a = +prompt('Enter the number');
+    let a = +prompt('Enter the first number');
+    let b = +prompt('Enter the second number');
 
-    let b = (a >= 1) ? 'Positive':
-    (a < 0) ? 'Negative' :
-    (a === 0) ? 'Zero' : 'www';
-    console.log('New value is ' + b);
+    let c = (a > b) ? (a + b) :
+    (a < b) ? (a * b) :
+    (a == b) ? 'These the same numbers' : 'ww';
+
+
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
+    // let b = (a >= 1) ? 'Positive':
+    // (a < 0) ? 'Negative' :
+    // (a === 0) ? 'Zero' : 'www';
+    // console.log('New value is ' + b);
    
 
 
