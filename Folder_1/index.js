ad419a04d0700016980652bdb973a525abb325ec
DmitryShastel <dmshastel@gmail.com>
2022-07-31 15:24:36 +0300

    F.lesson_JS_Operators task_39 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,4 +1,10 @@
-    let a = +prompt('Enter your date birthday', '');
-    let b = +prompt('Enter current year' , '');
-    console.log('Your age is ' +  (b - a));
+    let a = +prompt('Enter the number');
+
+    let b = (Number.isInteger(a)) ? (a * 2) : 'www'
+    console.log('New value is ' + b);
    
+
+
+
+    // let n = 123;
+    // console.log(Number.isInteger(n));
