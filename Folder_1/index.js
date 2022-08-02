dee7180477137e5bb55ff0130457bec8594a5714
DmitryShastel <dmshastel@gmail.com>
2022-08-02 18:51:30 +0300

    F.lesson_JS_Operators task_51 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,10 +1,6 @@
-let a = +prompt('The first catet');
+let a = +prompt('The first number');
+
+let c = (a < 0) ?  (a ** 2)  : 'error';
 
 
-let b = (a < 0) ?  Math.abs(a)  : (a = Number) ? 0 :'False';
-
-
-    console.log(a);
-    console.log(b);
-
-    // : (a == +a) ? 0 : (a === 0) ? 0 
+console.log(c);     
