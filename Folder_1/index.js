75410397940d097c27d54ad72ef1e84ad0eb2209
DmitryShastel <dmshastel@gmail.com>
2022-08-02 19:03:31 +0300

    F.lesson_JS_Operators task_52 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,6 +1,7 @@
 let a = +prompt('The first number');
+let b = +prompt('The second number');
 
-let c = (a < 0) ?  (a ** 2)  : 'error';
+let c = (a < b) ?  (-(a - b) + ' ' + 'Extra tables') : (a > b) ? (-(b - a) + ' ' + 'The chairs is not enough')  : 'error';
 
 
 console.log(c);     
