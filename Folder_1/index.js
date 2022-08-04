b7cbf4d99afd4ded2d65af65306d8ca08b213cd3
DmitryShastel <dmshastel@gmail.com>
2022-08-04 18:05:56 +0300

    F.lesson_JS_Operators task_58 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,8 +1,8 @@
 let a = +prompt('The first number');
 
-let c = (a === 1 || a === 2) ?  'неудовлетворительно' :
- (a === 3) ? 'удовлетворительно' : 
- (a === 4) ? 'хорошо' :
- (a === 5) ? 'отлично' : 'error';
+let c = (a == 12 || a ==  1 || a == 2) ?  'Winter' : 
+ (a == 3 || a ==  4 || a == 5) ? 'Spring' : 
+ (a == 6 || a ==  7 || a == 8) ? 'Summer' : 
+ (a == 9 || a ==  10 || a == 11) ? 'Autumn'  : 'error';
 
 console.log(c);  
