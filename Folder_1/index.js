94578a5e4390f95bb4ef9e6bf928fde6370f795b
DmitryShastel <dmshastel@gmail.com>
2022-08-04 20:22:52 +0300

    F.lesson_JS_Operators task_60 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,10 +1,5 @@
-let a = +prompt('The first number');
+let i = 3;
 
-let c = (a >= 6 && a <= 9) ?  'Morning' : 
- (a >= 10 && a <= 17) ? 'Noon' : 
- (a >= 18 && a <= 22) ? 'Evening' : 
- (a == 23 || a == 24) ? 'Night'  : 
- (a >= 1 && a <= 5) ? 'Night'  : 'error';
-
-console.log(c);  
-// a >= 23 &&
+while(i) {
+    console.log(i--);
+}
