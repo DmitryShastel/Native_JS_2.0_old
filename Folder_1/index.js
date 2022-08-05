0bde00d0bb9886dfaea16a7c56341b3e69504a40
DmitryShastel <dmshastel@gmail.com>
2022-08-05 18:56:06 +0300

    F.lesson_JS_Operators task_63 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,8 +1,12 @@
 let a = +prompt('The first number');
-let b = +prompt('The second number');
+let b = +prompt('The first number');
+let c = +prompt('The first number');
 
-let c = (a < b) ?  (a++ , b--) : (a > b) ? (b++, a--): (a === b) ? (a++, b++) :'error';
+let d = (a > 0 && a <= 89 && b > 0 && b <= 89 && c > 0 && c <= 89) ?  'Triangle' : 'error';
+let e = (a == 30 || 60 || 90  && b == 30 || 60 || 90  && c == 30 || 60 || 90) ?  'Triangle' : 'error';
+let f = (a > 90  || ( b > 90  || c > 90))  ?  'Triangle' : 'error';
 
 
-console.log(a);     
-console.log(b);
+console.log(d);  
+console.log(e);  
+console.log(f);
