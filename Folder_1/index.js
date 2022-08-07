72a4bede92de7e15374976a96b562db3b069cf45
DmitryShastel <dmshastel@gmail.com>
2022-08-07 12:16:57 +0300

    F.lesson_JS_Operators task_68 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,7 +1,18 @@
-let i = 0;
+// let i = +prompt('Enter number', '0');
 
-while(i < 3){
-    
-    console.log(`number ${i}!` )
-    i++
-}
+// while(i < 100) prompt('Enter the number again');
+// i = (i >= 100) ? console.log('Красава') :
+// i = (i == null) ? console.log('Canceled') : 'error2';
+
+
+// { 
+//     i =  (i < 100) ? prompt('Enter the number again') : 'error1'; 
+//     i = (i >= 100) ? console.log('Красава') :
+//     i = (i == null) ? console.log('Canceled') : 'error2';
+// }
+
+let num;
+
+do {
+    num = prompt('Enter the number more then 100', 0);
+} while (num <= 100 && num);
