19e05327011d59123aee0aa6500a4f4688415265
DmitryShastel <dmshastel@gmail.com>
2022-08-14 10:47:56 +0300

    F.lesson_JS_Operators task_78 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,63 +1,68 @@
-//Вывести числа от 4 до 400
-
-// let a = 4;
-
-// while(a <= 400) {
-//     console.log(a);
-//     a++;
+// let a = () => {
+//     console.log('Hello')
 // }
 
-//Вывести числа в последовательности: 4 7 10 13 с помощью цикла.
+// a();
 
-// let out = document.getElementById('out');
-// let tmp = '';
-
-// for (let i = 4; i <= 13; i= i + 3){
-//     tmp = tmp + i + ' ';
+// let showMessage = () => {
+//     let message = 'Hi I am JS';
+//     console.log(message);
 // }
 
-// console.log(tmp);
+// showMessage();
 
+// let userName = 'Vasa';
 
-//Вывести числа 654 653 652 до нуля
-
-// let tmp = '';
-
-// for (let i = 654; i >= 0; i--){
-//     tmp = tmp + i + ' ';
+// let showName = () => {
+//     let message = 'Hi ' + userName;
+//     console.log(message)
 // }
 
-// console.log(tmp);
+// showName();
 
+// let userName = 'Vasa';
 
-
-//Вывести все годы с 1983 по 2017
-// let tmp = '';
-
-// for (let i = 1983; i <= 2017; i++){
-//     tmp = tmp + i + ' ';
+// let showName = () => {
+//     let userName = 'Peta'
+//     let message = 'Hi ' + userName;
+//     console.log(message)
 // }
 
-// console.log(tmp);
 
+// showName();
+// console.log(userName)
 
-//Вывести числа -4 -2 0 2 4 6 .....100.
-
-// let tmp = '';
-
-// for (let i = -4; i <= 100; i++){
-//     tmp = tmp + i + ' ';
+// let showMessage = (from, text) => {
+//     console.log(from + ': ' + text);
 // }
 
-// console.log(tmp);
+// showMessage('Anna', 'Hi');
+// showMessage('Anna', 'How is going');
 
+// let showMessage = (from, text) => {
+//     from = '*' + from + '* ';
+//     console.log(from + ': ' + text); 
+// }
 
-//С помощью цикла напечатойте таблицу умножения на 7 от 1 до 9.
+// from = 'Anna';
+// showMessage(from, 'Hi');
 
-let tmp = '';
+// let sum = (a, b) => {
+//     return a + b;
+// }
 
-for (let i = 1; i <= 9; i++){
-    tmp = tmp + 7 * i + ' ';
+// let result = sum(1, 2);
+// console.log(result);
+
+let n = 5;
+
+let snowPrimes = (n) => {
+    nextPrime: for (let i = n; i < n; i++){
+        for(let j = 2; j < i; j++){
+            if(i % j == 0) continue nextPrime;
+        }
+        console.log(nextPrime)
+    }
 }
 
-console.log(tmp);
+snowPrimes(snowPrimes);
