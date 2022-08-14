7f4a29ecd3ee6a67611d178a18a64485c944c5f4
DmitryShastel <dmshastel@gmail.com>
2022-08-14 11:04:40 +0300

    F.lesson_JS_Operators task_79 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,68 +1,13 @@
-// let a = () => {
-//     console.log('Hello')
-// }
+let age = 20;
 
-// a();
-
-// let showMessage = () => {
-//     let message = 'Hi I am JS';
-//     console.log(message);
-// }
-
-// showMessage();
-
-// let userName = 'Vasa';
-
-// let showName = () => {
-//     let message = 'Hi ' + userName;
-//     console.log(message)
-// }
-
-// showName();
-
-// let userName = 'Vasa';
-
-// let showName = () => {
-//     let userName = 'Peta'
-//     let message = 'Hi ' + userName;
-//     console.log(message)
-// }
+let checkAge = (age) => {
+    
+    if(age > 18){
+        return console.log(true);
+      }else {
+        return console.log('False');
+      }    
+ }
 
 
-// showName();
-// console.log(userName)
-
-// let showMessage = (from, text) => {
-//     console.log(from + ': ' + text);
-// }
-
-// showMessage('Anna', 'Hi');
-// showMessage('Anna', 'How is going');
-
-// let showMessage = (from, text) => {
-//     from = '*' + from + '* ';
-//     console.log(from + ': ' + text); 
-// }
-
-// from = 'Anna';
-// showMessage(from, 'Hi');
-
-// let sum = (a, b) => {
-//     return a + b;
-// }
-
-// let result = sum(1, 2);
-// console.log(result);
-
-let n = 5;
-
-let snowPrimes = (n) => {
-    nextPrime: for (let i = n; i < n; i++){
-        for(let j = 2; j < i; j++){
-            if(i % j == 0) continue nextPrime;
-        }
-        console.log(nextPrime)
-    }
-}
-
-snowPrimes(snowPrimes);
+checkAge(age);
