269719f9ee413e9a4bf91f05d8697ca8a330b4a8
DmitryShastel <dmshastel@gmail.com>
2022-08-15 18:37:48 +0300

    F.lesson_JS_Operators task_85 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,8 +1,5 @@
-let getRandomInt = () => {
-    let min = 117;
-    let max = 132;
-    let ram = Math.floor(Math.random() * (max -min + 1)) + min;
-    console.log(ram)
+let showMessage = () => {
+    console.log('Hello')
 }
 
-getRandomInt();
+showMessage();
