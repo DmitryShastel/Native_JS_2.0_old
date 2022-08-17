0e67578b8c3dae2438e5f61625b324918e8277ce
DmitryShastel <dmshastel@gmail.com>
2022-08-17 15:40:22 +0300

    F.lesson_JS_Operators task_94 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,14 +1,5 @@
-//Calculate the user age
+let t1 = (a, b) => {
+    return console.log(a + b);
+}
 
-document.querySelector('.btn').addEventListener('click',
-function () {
-    const year = +document.querySelector('.inp').value;
-    if(isNaN(year)) return;
-    if(year <= 0 || year > 140) return;
-    document.querySelector('.out-1').innerHTML = 2022 - year;
-    console.log(year)
-});
-
-// let clearText = (data) => {
-//     return data.trim().toLowerCase();
-// }
+t1(1, 3)
