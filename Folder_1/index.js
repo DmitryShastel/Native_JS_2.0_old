fd51118821be7376e9eb863a33a513d3786ac702
DmitryShastel <dmshastel@gmail.com>
2022-08-18 17:56:01 +0300

    F.lesson_JS_Operators task_96 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,6 +1,18 @@
-let t2 = (a, b) => {
-    if(a < b) return b;
-    if(a > b) return a;
+//Вывести количество четных/нечетных чисел из числа n
+
+let num = () => {
+   
+    let a = document.getElementById('inp').value;
+    let count1 = 0;
+    let count2 = 0;
+
+    for (let i = 0; i < a.length; i++) {
+        if(a[i] % 2 == 0){ 
+            count1++;
+        } else 
+            count2++;
+        return console.log([count1, count2]);
+    } 
 }
 
-console.log(t2(3, 2));
+let b = num()
