540d55458cf5e9931bf862d06c022bca3e3f351b
DmitryShastel <dmshastel@gmail.com>
2022-08-18 17:56:54 +0300

    F.lesson_JS_Operators task_97 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,18 +1,10 @@
-//Вывести количество четных/нечетных чисел из числа n
-
-let num = () => {
-   
-    let a = document.getElementById('inp').value;
-    let count1 = 0;
-    let count2 = 0;
-
-    for (let i = 0; i < a.length; i++) {
-        if(a[i] % 2 == 0){ 
-            count1++;
-        } else 
-            count2++;
-        return console.log([count1, count2]);
-    } 
+let obj = {
+    name: 'Ivan',
+    age: 30,
+    hello() {
+        console.log('My name is ' + this.name + ' I am ' + this.age);
+ }
 }
+// obj.hello();
 
-let b = num()
+obj.hello();
