65ff0f37a6c3862f5d364f425b64699c90e817f0
DmitryShastel <dmshastel@gmail.com>
2022-08-23 19:08:46 +0300

    F.lesson_JS_Operators task_106 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,21 +1,12 @@
-//Проверить является ли строка палиндромом 
+let inputIn = document.querySelector('#inp');
+let button = document.querySelector('.btn');
+let div = document.querySelector('#out-1');
 
-let palindrome = () => {
-  
-    let str = document.getElementById('inp').value.toLowerCase();
-
-    return console.log(str.split('').reverse().join('') == str);
-    
+button.onclick = function ()  {
+    console.log('Hello!');
+    // console.log(inputIn.value);
+    let a = +inputIn.value;
+    console.log(a + 10);
+    div.innerHTML = a + 10;
+    inputIn.value = '';
 }
-
-
-
-// let palindrome = () => {
-//     let str = document.getElementById('inp').value.toLowerCase();
-//     let str2 = str.split('');
-//     str2 = str2.reverse();
-//     str2 = str2.join('');
-//     if(str == str2) return console.log(true);
-//     else return console.log(false);
-
-// }
