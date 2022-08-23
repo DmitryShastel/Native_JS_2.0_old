e5ca71676e2e8aa771163af87989ca6f1c4a7a05
DmitryShastel <dmshastel@gmail.com>
2022-08-23 19:42:46 +0300

    F.lesson_JS_Operators task_111 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,5 +1,5 @@
-let arr = ['a', 'b', 1, 2, true, false];
+let arr = [2, 'hello', 17, 34, 'hi'];
 
 // document.querySelector('#out-1').innerHTML = arr;
 
-console.log(arr[0], arr[3], arr[8]);
+console.log(arr[0] + arr[2] + arr[3]);
