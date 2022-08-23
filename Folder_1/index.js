f005be0e4554a60be0b5bfe43ab5ae4710d2b3fe
DmitryShastel <dmshastel@gmail.com>
2022-08-23 19:29:33 +0300

    F.lesson_JS_Operators task_107 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,12 +1,3 @@
-let inputIn = document.querySelector('#inp');
-let button = document.querySelector('.btn');
-let div = document.querySelector('#out-1');
+let arr = ['a', 'b', 1, 2, true, false];
 
-button.onclick = function ()  {
-    console.log('Hello!');
-    // console.log(inputIn.value);
-    let a = +inputIn.value;
-    console.log(a + 10);
-    div.innerHTML = a + 10;
-    inputIn.value = '';
-}
+console.log(arr);
