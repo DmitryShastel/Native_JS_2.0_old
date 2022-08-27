1d63dbf1884843dc2f032d10f3ab4e2df3864884
DmitryShastel <dmshastel@gmail.com>
2022-08-27 20:29:45 +0300

    F.lesson_JS_Operators task_112 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,18 +1,8 @@
-//Что выведет следующий код?
+let inputIn = document.querySelector('#inp');
+let arr = [];
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-let arr2 = arr;
-arr.push('a');
-
-console.log(arr);
-
-let arr = ['Jaz', 'Blyz'];
-
-arr[1] = 'classika'
-arr.push('Rock');
-arr.shift();
-arr.unshift('Reggy');
-arr.unshift('Rap');
-
-console.log(arr);
+let fill = () => {
+    arr.push(inputIn.value);  
+    console.log(arr);
+    inputIn.value = '';
+}
