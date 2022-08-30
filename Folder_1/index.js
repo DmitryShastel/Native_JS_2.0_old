caa67f1db0f3d58b3fc2b93ba0cd22bfb7003919
DmitryShastel <dmshastel@gmail.com>
2022-08-30 21:09:20 +0300

    F.lesson_JS_Operators task_129 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,15 +1,12 @@
-// Сумма свойств объекта
 let user = {
-  Jonh: 100,
-  Ann: 160,
-  Pete: 130,
-  title: "My menu",
+  'Jonh': 100,
+  'Ann': 160,
+  'Pete': 130,
 };
 
+console.log(user);
+
 let schedule = () => {
-  for (let key in user)
-    if (typeof user[key] == "number") {
-      user[key] *= 2;
-      console.log(user);
-    }
-};
+  let sum = user.Jonh + user.Ann + user.Pete;
+  console.log(sum);
+}
