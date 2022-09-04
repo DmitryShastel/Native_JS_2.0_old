22cb196b84ab2a02ce9b51134a2c839fa49496a1
DmitryShastel <dmshastel@gmail.com>
2022-09-04 19:39:06 +0300

    F.lesson_JS_Array task_151 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -19,22 +19,25 @@ const users = [
     first_name: "Mike",
     last_name: "Sheridan",
     age: 30,
+    location: "London",
   },
   {
     first_name: "Tim",
     last_name: "Lee",
     age: 22,
+    location: "US",
   },
   {
     first_name: "John",
     last_name: "Carte",
     age: 45,
+    location: "Australia",
   },
 ];
 
 let showUsers = () => {
   const transformObject = users.map((user) => {
-    return `${user.age}`;
+    return `${user.first_name} lives in ${user.location}`;
   });
   console.log(users, transformObject);
 };
