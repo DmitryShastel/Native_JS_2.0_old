0c352aff8db5964147f5610b82c982495f83c8a7
DmitryShastel <dmshastel@gmail.com>
2022-09-04 19:29:00 +0300

    F.lesson_JS_Array task_148 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,7 +1,37 @@
 const months = ["January", "February", "March", "Aprial"];
 
-const transformedArray = months.map((months) => {
-  return months.toUpperCase();
-});
+// const transformedArray = months.map((months) => {
+//   return months.toUpperCase();
+// });
 
-console.log(transformedArray);
+// console.log(months, transformedArray);
+
+let showArray = () => {
+  const transformedArray = months.map((month) => {
+    return month.toUpperCase();
+  });
+
+  console.log(months, transformedArray);
+};
+
+// const users = [
+//     {
+//         first_name: 'Mike',
+//         last_name: 'Sheridan',
+//     },
+//     {
+//         first_name: 'Tim',
+//         last_name: 'Lee',
+//     },
+//     {
+//         first_name: 'John',
+//         last_name: 'Carte',
+//     }
+// ];
+
+// let showUsers = () => {
+//     const transformObject = users.map((user) => {
+//         return user.first_name + ' ' + user.last_name;
+//     })
+//     console.log(users, transformObject)
+// }
