55b48da5b92e38bfeded3551bde5ed7a8901a9f6
DmitryShastel <dmshastel@gmail.com>
2022-09-04 19:32:00 +0300

    F.lesson_JS_Array task_149 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,4 +1,4 @@
-const months = ["January", "February", "March", "Aprial"];
+// const months = ["January", "February", "March", "Aprial"];
 
 // const transformedArray = months.map((months) => {
 //   return months.toUpperCase();
@@ -6,32 +6,32 @@ const months = ["January", "February", "March", "Aprial"];
 
 // console.log(months, transformedArray);
 
-let showArray = () => {
-  const transformedArray = months.map((month) => {
-    return month.toUpperCase();
+// let showArray = () => {
+//   const transformedArray = months.map((month) => {
+//     return month.toUpperCase();
+//   });
+
+//   console.log(months, transformedArray);
+// };
+
+const users = [
+  {
+    first_name: "Mike",
+    last_name: "Sheridan",
+  },
+  {
+    first_name: "Tim",
+    last_name: "Lee",
+  },
+  {
+    first_name: "John",
+    last_name: "Carte",
+  },
+];
+
+let showUsers = () => {
+  const transformObject = users.map((user) => {
+    return `${user.first_name} ${user.first_name}`;
   });
-
-  console.log(months, transformedArray);
+  console.log(users, transformObject);
 };
-
-// const users = [
-//     {
-//         first_name: 'Mike',
-//         last_name: 'Sheridan',
-//     },
-//     {
-//         first_name: 'Tim',
-//         last_name: 'Lee',
-//     },
-//     {
-//         first_name: 'John',
-//         last_name: 'Carte',
-//     }
-// ];
-
-// let showUsers = () => {
-//     const transformObject = users.map((user) => {
-//         return user.first_name + ' ' + user.last_name;
-//     })
-//     console.log(users, transformObject)
-// }
