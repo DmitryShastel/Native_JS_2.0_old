c41b614d4440fb52d26c85ed308c046859171e58
DmitryShastel <dmshastel@gmail.com>
2022-09-05 19:39:45 +0300

    F.lesson_JS_Array task_152 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -14,30 +14,57 @@
 //   console.log(months, transformedArray);
 // };
 
-const users = [
-  {
-    first_name: "Mike",
-    last_name: "Sheridan",
-    age: 30,
-    location: "London",
-  },
-  {
-    first_name: "Tim",
-    last_name: "Lee",
-    age: 22,
-    location: "US",
-  },
-  {
-    first_name: "John",
-    last_name: "Carte",
-    age: 45,
-    location: "Australia",
-  },
-];
+// const users = [
+//   {
+//     first_name: "Mike",
+//     last_name: "Sheridan",
+//     age: 30,
+//     location: "London",
+//   },
+//   {
+//     first_name: "Tim",
+//     last_name: "Lee",
+//     age: 22,
+//     location: "US",
+//   },
+//   {
+//     first_name: "John",
+//     last_name: "Carte",
+//     age: 45,
+//     location: "Australia",
+//   },
+// ];
 
-let showUsers = () => {
-  const transformObject = users.map((user) => {
-    return `${user.first_name} lives in ${user.location}`;
-  });
-  console.log(users, transformObject);
-};
+// let showUsers = () => {
+//   const transformObject = users.map((user) => {
+//     return `${user.first_name} lives in ${user.location}`;
+//   });
+//   console.log(users, transformObject);
+// };
+
+//перевернуть символы каждой строки в обратном порядке.
+// const strings = ["regardless", "hello"];
+
+// let newStrings = strings.map((string) => {
+//   return string.split("").reverse().join("");
+// });
+
+// console.log(newStrings);
+
+// const arr = ["123", "456", "789"];
+
+// let arr1 = [];
+
+// let newArr = arr.map((e) =>
+//   e.split("").map((singleNumber) => Number(singleNumber))
+// );
+
+// console.log(newArr);
+
+let numbers = [1, 2, 3, 4, 5];
+
+let arrayCopy = numbers.map((numbers) => {
+  return numbers;
+});
+
+console.log(arrayCopy);
