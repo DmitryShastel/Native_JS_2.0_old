d58ba364824223d608938b6f9fbbda21798aaefa
DmitryShastel <dmshastel@gmail.com>
2022-09-06 19:15:12 +0300

    F.lesson_JS_Array task_154 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,91 +1,12 @@
-// const months = ["January", "February", "March", "Aprial"];
-
-// const transformedArray = months.map((months) => {
-//   return months.toUpperCase();
-// });
-
-let studentRecords = [
-  {
-    id: 1,
-    name: "John",
-    marks: 98,
-  },
-  {
-    id: 2,
-    name: "Baba",
-    marks: 23,
-  },
-  {
-    id: 3,
-    name: "yaga",
-    marks: 45,
-  },
-  {
-    id: 4,
-    name: "Wick",
-    marks: 75,
-  },
+const employees = [
+  { name: "David", age: 30 },
+  { name: "John", age: 28 },
+  { name: "Mike", age: 36 },
+  { name: "John", age: 30 },
 ];
 
-let studentNames = studentRecords.map((studentRecords) =>
-  studentRecords.name.toUpperCase()
+let employee = employees.filter(
+  (employee) => employee.name.indexOf("John") > -1
 );
 
-console.log(studentNames);
-
-// console.log(months, transformedArray);
-
-// let showArray = () => {
-//   const transformedArray = months.map((month) => {
-//     return month.toUpperCase();
-//   });
-
-//   console.log(months, transformedArray);
-// };
-
-// const users = [
-//   {
-//     first_name: "Mike",
-//     last_name: "Sheridan",
-//     age: 30,
-//     location: "London",
-//   },
-//   {
-//     first_name: "Tim",
-//     last_name: "Lee",
-//     age: 22,
-//     location: "US",
-//   },
-//   {
-//     first_name: "John",
-//     last_name: "Carte",
-//     age: 45,
-//     location: "Australia",
-//   },
-// ];
-
-// let showUsers = () => {
-//   const transformObject = users.map((user) => {
-//     return `${user.first_name} lives in ${user.location}`;
-//   });
-//   console.log(users, transformObject);
-// };
-
-//перевернуть символы каждой строки в обратном порядке.
-// const strings = ["regardless", "hello"];
-
-// let newStrings = strings.map((string) => {
-//   return string.split("").reverse().join("");
-// });
-
-// console.log(newStrings);
-
-// const arr = ["123", "456", "789"];
-
-// let arr1 = [];
-
-// let newArr = arr.map((e) =>
-//   e.split("").map((singleNumber) => Number(singleNumber))
-// );
-
-// console.log(newArr);
+console.log(employee);
