acc50447685bfd2f65e71c26959496aefb364abd
DmitryShastel <dmshastel@gmail.com>
2022-09-06 19:35:07 +0300

    F.lesson_JS_Array task_157 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,11 +1,5 @@
-let arr = [1, 2, 3, 4, 5, 6];
+let arr = [1, 2, 3, -5, 4, 5, 6, -8];
 
-let arr2 = arr.filter((e) => {
-  if (e % 2 == 0) {
-    return true;
-  } else {
-    return false;
-  }
-});
+let arr2 = arr.filter((e) => e >= 0);
 
 console.log(arr2);
