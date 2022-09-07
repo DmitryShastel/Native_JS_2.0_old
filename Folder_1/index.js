2c57942fd2ffd1ce6aee180b27b5fdd8b88c119c
DmitryShastel <dmshastel@gmail.com>
2022-09-07 22:01:52 +0300

    F.lesson_JS_Array task_149 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,8 +1,3 @@
-const numbers = [1, 2, 3, 4, 5];
-
-let newNumbers = numbers.map((number) => number * number);
-console.log(numbers, newNumbers);
-
 const strings = ["a", "b", "c", "d", "e"];
 
 let newStrings = strings.map((string) => {
