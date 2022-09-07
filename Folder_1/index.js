790a3256c5995926f74150ef6ad5ac49e8961ac6
DmitryShastel <dmshastel@gmail.com>
2022-09-07 22:00:23 +0300

    F.lesson_JS_Array task_147 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,5 +1,5 @@
-let arr = [1, 2, 3, 4, 5, -7];
+let arr = [1, 2, 3, -5, 4, 5, 6, -8, 11, 20, 0, "qa", "qwerty", "asd"];
 
-let arr2 = arr.filter((e) => e < 0);
+let arr2 = arr.filter((e) => e.length > 2);
 
-console.log(arr2.length);
+console.log(arr2);
