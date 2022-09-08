5cabff40065b9535b2521e7fcabe20d995bbe857
DmitryShastel <dmshastel@gmail.com>
2022-09-08 18:24:28 +0300

    F.lesson_JS_Array task_150 is done

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -1,7 +1,7 @@
-const strings = ["a", "b", "c", "d", "e"];
+const strings = [1, 2, 3, 4];
 
-let newStrings = strings.map((string) => {
-  return string + " " + "!";
+let newStrings = strings.map((string, index) => {
+  return string * index;
 });
 
-console.log(newStrings);
+console.log(newStrings); // [0, 2, 6, 12]
