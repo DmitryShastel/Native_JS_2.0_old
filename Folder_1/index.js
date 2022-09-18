ae28e8b2c348e3909de86b25f2d1afd81cec56f2
DmitryShastel <dmshastel@gmail.com>
2022-09-18 10:48:59 +0300

    F.lesson_JS_Calculator number buttons

--- Folder_1/index.js
+++ Folder_1/index.js
@@ -18,3 +18,30 @@ let clearAll = () => {
 };
 
 document.querySelector(".ac").onclick = clearAll;
+
+document.querySelector(".buttons").onclick = (event) => {
+  //the button ac isn't pushed
+  if (!event.target.classList.contains("btn")) return;
+  //the button ac is pushed
+  if (event.target.classList.contains("ac")) return;
+
+  out.textContent = "";
+
+  //get pushed button
+  const key = event.target.textContent;
+
+  //if pushed button 0-9 or dot
+  if (digit.includes(key)) {
+    a += key;
+    console.log(a, b, sign);
+    out.textContent = a;
+  }
+
+  //if pushed the button + - / *
+  if (action.includes(key)) {
+    sing = key;
+    out.textContent = sign;
+    console.log(a, b, sing);
+    return;
+  }
+};
