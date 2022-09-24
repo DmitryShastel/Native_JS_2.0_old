d1759efc9f765508120b6f10ef1f3e5851939a33
DmitryShastel <dmshastel@gmail.com>
2022-09-24 20:23:34 +0300

    The unit_test_№2 is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/Unit_Tests_Monday/monday.test.ts
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/Unit_Tests_Monday/monday.test.ts
@@ -5,13 +5,24 @@ test('sum should be correct', () => {
     //data
     const a = 1;
     const b = 1;
-    const c = 1;
 
     //action
     const result = sum(a,b);
 
     //expected result
     expect(result).toBe(2);
+})
+
+test('mult should be correct', () => {
+    //data
+    const a = 2;
+    const b = 2;
+
+    //action
+    const result = sum(a,b);
+
+    //expected result
+    expect(result).toBe(4);
 })
 
 
