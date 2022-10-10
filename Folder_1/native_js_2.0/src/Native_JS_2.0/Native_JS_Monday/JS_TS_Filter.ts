10c2e50410ed6baf4987007be227f4bd95001a9c
DmitryShastel <dmshastel@gmail.com>
2022-10-10 09:48:39 +0300

    The NJ_#4 task#1 is done

---
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/JS_TS_Filter.ts
@@ -0,0 +1,13 @@
+const ages = [18, 20, 22, 1, 100, 90, 14];
+
+export let sortAges = () => {
+    let oldAges = ages.filter((e) => {
+        return e > 90
+    });
+console.log(oldAges)
+}
+
+
+
+
+
--- Folder_1/native_js_2.0/src/index.tsx
+++ Folder_1/native_js_2.0/src/index.tsx
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {changeName, copyUsers, newUser, user, users} from "./Native_JS_2.0/Native_JS_Monday/JS_TS_Objects";
+import {sortAges} from "./Native_JS_2.0/Native_JS_Monday/JS_TS_Filter";
 
 
-console.log(changeName)
+sortAges();
+
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
