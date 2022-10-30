2f17ba12cf5f08eeb786519cc163b980bc19307a
DmitryShastel <dmshastel@gmail.com>
2022-10-30 09:37:07 +0300

    The NJ№7 task#6 is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/Unit_Tests_Monday/monday.test.ts
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/Unit_Tests_Monday/monday.test.ts
@@ -9,6 +9,41 @@ import {
     toFireStaff, toHireStaff
 } from "../JS_TS_Functions";
 
+type ManType = {
+    name: string
+    age: number
+    lessons: Array<{title: string}>
+}
+
+let props: ManType;
+
+beforeEach(() => {
+    props = {
+        name: 'Dimych',
+        age: 32,
+        lessons: [{title: '1'}, {title:'2'}]
+    }
+})
+
+test('', () => {
+    // const age = props.age
+    // const lessons = props.lessons
+    const{age, lessons} = props
+
+    const a = props.age
+    const l = props.lessons
+
+    expect(age).toBe(32)
+    expect(lessons.length).toBe(2)
+
+    expect(a).toBe(32)
+    expect(l.length).toBe(2)
+})
+
+
+
+
+
 
 //tests for Test_Functions
 let a: number;
@@ -306,30 +341,7 @@ test.skip('get only completed tasks', ()=> {
 })
 
 
-test('', () => {
-        // const age = props.age
-    // const lessons = props.lessons
-    const{age, lessons} = props
 
-    const a = props.age
-    const l = props.lessons
-
-    expect(age).toBe(32)
-    expect(lessons.length).toBe(2)
-
-    expect(a).toBe(32)
-    expect(l.length).toBe(2)
-})
-
-let props;
-
-beforeEach(() => {
-    props = {
-        name: 'Dimych',
-        age: 32,
-        lessons: [{title: '1'}, {title:'2'}]
-    }
-})
 
 
 
