e2419b6a9ccd13824cb8c3f06fe0d694aed31ed1
DmitryShastel <dmshastel@gmail.com>
2022-10-30 09:30:18 +0300

    The NJ№7 task#5 is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/Unit_Tests_Monday/monday.test.ts
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/Unit_Tests_Monday/monday.test.ts
@@ -306,6 +306,32 @@ test.skip('get only completed tasks', ()=> {
 })
 
 
+test('', () => {
+        // const age = props.age
+    // const lessons = props.lessons
+    const{age, lessons} = props
+
+    const a = props.age
+    const l = props.lessons
+
+    expect(age).toBe(32)
+    expect(lessons.length).toBe(2)
+
+    expect(a).toBe(32)
+    expect(l.length).toBe(2)
+})
+
+let props;
+
+beforeEach(() => {
+    props = {
+        name: 'Dimych',
+        age: 32,
+        lessons: [{title: '1'}, {title:'2'}]
+    }
+})
+
+
 
 
 
