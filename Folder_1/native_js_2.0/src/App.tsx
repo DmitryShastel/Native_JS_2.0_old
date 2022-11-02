036d2a64f40e6b21edbf792bd7ed888c92d63b2c
DmitryShastel <dmshastel@gmail.com>
2022-11-02 10:40:30 +0300

    The NJ the component is created

--- Folder_1/native_js_2.0/src/App.tsx
+++ Folder_1/native_js_2.0/src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+
 
 
 function App() {
+    let [message, setMessage] = useState([
+        {message: 'message1'},
+        {message: 'message2'},
+        {message: 'message3'},
+    ])
+
     return (
         <div className="App">
+            <div className='block'>
+                <input/>
+                <button className='button'>+</button>
+                {message.map((el, index) => {
+                    return (
+                        <div key={index}>{el.message} </div>
+                    )
+                })}
+            </div>
 
 
         </div>
