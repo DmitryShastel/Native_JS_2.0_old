9d879c87ac7174e9e630c2315307d851ee63929b
DmitryShastel <dmshastel@gmail.com>
2022-11-03 11:24:47 +0300

    The buttons is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/useState.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/useState.tsx
@@ -5,8 +5,11 @@ export const UseStateComponent = () => {
     const [count, setCount] = useState(0)
 
     return (
-        <div>
+        <div >
             {count}
+            <button onClick={() => setCount(0)} className='buttons'>Reset</button>
+            <button onClick={() => setCount(count => count + 1)} className='buttons'>+</button>
+            <button onClick={() => setCount(count => count - 1)}>-</button>
         </div>
     );
 };
