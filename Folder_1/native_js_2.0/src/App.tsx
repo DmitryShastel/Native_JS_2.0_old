2b72dae478822c40a312d61e938e6f2feafd649d
DmitryShastel <dmshastel@gmail.com>
2022-11-08 08:35:28 +0300

    The useState for input is done

--- Folder_1/native_js_2.0/src/App.tsx
+++ Folder_1/native_js_2.0/src/App.tsx
@@ -4,24 +4,25 @@ import {FullInput} from "./Native_JS_2.0/Native_JS_Monday/Fullinput";
 
 function App() {
     let [task, setTask] = useState([
-        {id: 1, name: 'htm & css', priority: 'low'},
-        {id: 2, name: 'js', priority: 'high'},
-        {id: 3, name: 'react', priority: 'middle'},
-        {id: 4, name: 'sql', priority: 'middle'},
-        {id: 5, name: 'git', priority: 'low'},
-        {id: 6, name: 'typescript', priority: 'high'},
+        { name: 'htm & css'},
+        { name: 'js'},
+        { name: 'react'},
+        { name: 'sql'},
+        { name: 'git'},
+        { name: 'typescript'}
 
     ])
 
-
+    const addNewTask = (title: string) => {
+        let newTitle = { name: title}
+        setTask([newTitle, ...task])
+    }
 
     return (
         <div className="App">
 
 
-            <FullInput />
-
-
+            <FullInput addNewTask={addNewTask}/>
 
 
             <div className='block'>
--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/Fullinput.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/Fullinput.tsx
@@ -1,15 +1,29 @@
-import React, {useState} from "react"
+import React, {ChangeEvent, useState} from "react"
 
 
-export const FullInput = () => {
+type FullInputType = {
+    addNewTask: (title: string) => void
+}
+
+export const FullInput = (props: FullInputType) => {
 
     let [title, setTitle] = useState('')
+
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
+
+    const onClickHandler = () => {
+        props.addNewTask(title)
+        setTitle('')
+    }
 
     return (
         <div className='block'>
             <div>
-                <input  onChange={() => {}}/>
-                <button>+</button>
+                <input value={title} onChange={onChangeHandler}/>
+                <button onClick={onClickHandler}>+</button>
             </div>
         </div>
     )
