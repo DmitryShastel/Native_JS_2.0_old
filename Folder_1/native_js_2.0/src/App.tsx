2bb9e0011a3424b645bf5eb3a9959d06418d2d26
DmitryShastel <dmshastel@gmail.com>
2022-11-11 04:44:38 +0300

    The source fike is created

--- Folder_1/native_js_2.0/src/App.tsx
+++ Folder_1/native_js_2.0/src/App.tsx
@@ -1,56 +1,13 @@
-import React, {useState} from 'react';
-import {FullInput, TaskType} from "./Native_JS_2.0/Native_JS_Monday/Fullinput";
-
-
-export type FilterType = 'all' | 'completed' | 'active';
+import React from 'react';
 
 function App() {
-    let [filter, setFilter] = useState<FilterType>('all')
-    let [tasks, setTasks] = useState<Array<TaskType>>([
-        {id: 1, title: 'message1', isDone: true},
-        {id: 2, title: 'message2', isDone: true},
-        {id: 3, title: 'message3', isDone: false},
-        {id: 4, title: 'message4', isDone: false},
-        {id: 5, title: 'message5', isDone: true},
 
-    ])
-
-
-    let removeTask = (id: number) => {
-        let removedTask = tasks.filter(t => t.id !== id)
-        setTasks(removedTask)
-    }
-
-    let taskForTodolist = tasks;
-    if (filter === 'active') {
-         taskForTodolist = tasks.filter(t => t.isDone === false)
-    }
-    if (filter === 'completed') {
-         taskForTodolist = tasks.filter(t => t.isDone === true)
-    }
-
-    let filteredTasks = (value: FilterType) => {
-        setFilter(value)
-    }
-
-    // let filteredTasks = (value: FilterType) => {
-    //     setFilter(value)
-    // }
-
+   
     return (
         <div className="App">
             <div className='block'>
-                <FullInput
-                    tasks={taskForTodolist}
-                    title='Tasks'
-                    removeTask={removeTask}
-                    filteredTasks={filteredTasks}
-                />
+
             </div>
-
-
-            {/*<UseStateComponent1/>*/}
-            {/*<UseStateComponent2/>*/}
         </div>
     )
 }
--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/Fullinput.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/Fullinput.tsx
@@ -1,49 +1,12 @@
 import React from "react"
-import {FilterType} from "../../App";
 
 
- export type TaskType = {
-    id: number
-    title: string
-    isDone: boolean
-}
-
-type FullInputType = {
-    title: string
-    tasks: Array<TaskType>
-    removeTask: (id: number) => void
-    filteredTasks: (value: FilterType) => void
-}
-
-
-export const FullInput = (props: FullInputType) => {
+export const FullInput = () => {
 
 
     return (
         <div className='block'>
-            <div>
-                <h3>{props.title}</h3>
-                <input/>
-                <button>+</button>
-                <ul>
-                    {
-                        props.tasks.map((t) => {
-                            return <li key={t.id}>
-                                <input type='checkbox' checked={t.isDone}/>
-                                <span>{t.title}</span>
-                                <button onClick={() => {
-                                    props.removeTask(t.id)}}>x
-                                </button>
-                            </li>
-                        })
-                    }
-                </ul>
-                <div>
-                    <button onClick={()=> {props.filteredTasks('all')}}>All</button>
-                    <button onClick={()=> {props.filteredTasks('active')}}>Active</button>
-                    <button onClick={()=> {props.filteredTasks('completed')}}>Completed</button>
-                </div>
-            </div>
+
 
         </div>
     )
