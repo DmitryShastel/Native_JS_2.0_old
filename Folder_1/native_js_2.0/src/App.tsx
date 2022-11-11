36f546e92bf79ee8ce69bd176ec21f033ba4cfc9
DmitryShastel <dmshastel@gmail.com>
2022-11-11 05:58:32 +0300

    The function removeTasks is created

--- Folder_1/native_js_2.0/src/App.tsx
+++ Folder_1/native_js_2.0/src/App.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ToDolist} from "./Native_JS_2.0/Native_JS_Monday/Fullinput";
 
 function App() {
 
-    let tasks = [
+    let [tasks, setTasks] = useState([
         {id: 1, title: 'HTML&CSS', isDone: false},
         {id: 2, title: 'JS', isDone: true},
         {id: 3, title: 'React', isDone: true},
         {id: 4, title: 'SQL', isDone: false},
         {id: 5, title: 'MongoBD', isDone: true},
         {id: 6, title: 'English', isDone: false}
-    ]
+    ])
+
+    const removeTask = () => {
+
+    }
 
 
     return (
