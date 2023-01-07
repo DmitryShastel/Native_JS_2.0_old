a34b459e65dd307a8c11eb213c711156725e6a26
DmitryShastel <dmshastel@gmail.com>
2023-01-07 10:58:24 +0300

    The reduce 3 is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/script.js
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/script.js
@@ -61,6 +61,38 @@ console.log([...tasks[todoListId_1], {name: 'React', isDone: true}])
 
 // reduce: [1,2,3,4,5,6] => @
 
+const students = [
+    {
+        id: 1,
+        name: 'Bob',
+        age: 22,
+        isMarried: true,
+        scores: 85,
+    },
+    {
+        id: 2,
+        name: 'Alex',
+        age: 29,
+        isMarried: true,
+        scores: 89,
+    },
+    {
+        id: 3,
+        name: 'Nick',
+        age: 20,
+        isMarried: false,
+        scores: 120,
+    },
+    {
+        id: 4,
+        name: 'John',
+        age: 22,
+        isMarried: false,
+        scores: 100,
+    }
+]
+
+
 const nums = [1, 2, 3, 4, 5, 6]
 
 console.log(nums.reduce((acc, el) => {
@@ -71,7 +103,9 @@ console.log(nums.reduce((acc, el) => {
     return acc > el ? acc : el
 }, 0))
 
-
+console.log(students.reduce((acc, el) => {
+    return acc + el.scores
+}, 0))
 
 
 
