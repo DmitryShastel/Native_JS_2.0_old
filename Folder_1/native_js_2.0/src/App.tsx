d6251e7334c37d85c6ac04964f4090b23df8ede6
DmitryShastel <dmshastel@gmail.com>
2023-01-30 18:07:06 +0300

    The useState is created

--- Folder_1/native_js_2.0/src/App.tsx
+++ Folder_1/native_js_2.0/src/App.tsx
@@ -5,7 +5,12 @@ import {ToDoList} from "./Native_JS_2.0/Native_JS_Monday/ToDoList/ToDoList";
 
 function App() {
 
-    let [task, setTask] = useState('')
+    let [task, setTask] = useState([
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false},
+        {id: 4, title: 'TypeScript', isDone: false}
+    ])
 
     return (
         <div className="App">
