e56e3498bf5fbf5c05f8e29940c8f09e0720f568
DmitryShastel <dmshastel@gmail.com>
2023-02-04 11:28:05 +0300

    The useStates is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/Components/AddUser.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Monday/Components/AddUser.tsx
@@ -1,20 +1,31 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
+import {v1} from "uuid";
 
 
-
+type UserType = {
+    id: string
+    name: string
+}
 
 
 export const AddUser = () => {
 
-    let [users, setUsers] = useState([])
+    let [users, setUsers] = useState<UserType[]>([])
+    let [name, setName] = useState('')
 
     const totalUsers = users.length
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value)
+    }
 
-
+    const addUser = () => {
+        let user = {id: v1(), name: name}
+        setUsers([...users, user])
+    }
 
     return (
         <div>
-            <input/>
+            <input value={name} onChange={onChangeHandler}/>
             <button>Add</button>
             <div>Count:{totalUsers}</div>
         </div>
