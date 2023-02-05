24288de6df8066ac7e838444cb9b50a31479520a
DmitryShastel <dmshastel@gmail.com>
2023-02-05 10:35:51 +0300

    The second method is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/main.js
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/main.js
@@ -1,4 +1,9 @@
 //1 this method is sorting out of the box without additional parameters
 
-const str = ['Bob', 'Alex', 'John']
-console.log(str.sort()) //['Alex', 'Bob', 'John']
+const str1 = ['Bob', 'Alex', 'John']
+console.log(str1.sort()) //['Alex', 'Bob', 'John']
+
+//2 it's sorts type strings alphabetically
+
+const str2 = ['Bob', 'Alex', 'John', 'john', '123', 'игорь', 'Юрий']
+console.log(str2.sort()) //['123', 'Alex', 'Bob', 'John', 'john', 'Юрий', 'игорь']
