1df1e02a9d8ae0c29f09f8ee0c41b9ab090067c5
DmitryShastel <dmshastel@gmail.com>
2023-02-05 11:12:22 +0300

    The two funs are created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/main.js
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/main.js
@@ -7,3 +7,82 @@ console.log(str1.sort()) //['Alex', 'Bob', 'John']
 
 const str2 = ['Bob', 'Alex', 'John', 'john', '123', 'игорь', 'Юрий']
 console.log(str2.sort()) //['123', 'Alex', 'Bob', 'John', 'john', 'Юрий', 'игорь']
+
+
+const students = [
+    {
+        id: 1,
+        name: 'Bob',
+        age: 22,
+        isMarried: true,
+        scores: 85,
+    },
+    {
+        id: 2,
+        name: 'Alex',
+        age: 29,
+        isMarried: true,
+        scores: 89,
+    },
+    {
+        id: 3,
+        name: 'Nick',
+        age: 20,
+        isMarried: false,
+        scores: 120,
+    },
+    {
+        id: 4,
+        name: 'John',
+        age: 22,
+        isMarried: false,
+        scores: 100,
+    },
+    {
+        id: 5,
+        name: 'alex',
+        age: 23,
+        isMarried: false,
+        scores: 105,
+    }
+]
+
+//сортировка массива объектов по строковым значениям
+//а. Регистрозависимоя
+
+const compareObjByName = (a, b) => {
+    if(a.name > b.name) {
+        return 1
+    } else {
+        return -1
+    }
+}
+
+console.log(students.sort(compareObjByName))
+
+//б. РегистроНЕзависимоя
+console.log(students.sort((a,b) => a.name.localeCompare(b.name)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
