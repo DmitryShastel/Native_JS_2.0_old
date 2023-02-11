856f87ee6228153a1b05093c304b94b3ca8f4181
DmitryShastel <dmshastel@gmail.com>
2023-02-11 10:54:47 +0300

    The native_practice_task task#1 is done

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/main.js
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/main.js
@@ -1,183 +1,193 @@
-// <script>
-//     // links to watch theory
-//     // https://www.youtube.com/watch?v=6napu-MGQDo&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=47
-//     // https://www.youtube.com/watch?v=I8LNJpG60vI&feature=youtu.be
-//
-//     // 1. Simple object
-//     let man = {
-//     name: 'John',
-//     age: 28
-// };
-//
-//     let manFullCopy  //  your code
-//
-//
-//     // 2. Array of primitives
-//     let numbers = [1, 2, 3];
-//
-//     let numbersFullCopy  //  your code
-//
-//
-//     // 3. Object inside an object
-//     let man1 = {
-//     name: 'John',
-//     age: 28,
-//     mother: {
-//     name: 'Kate',
-//     age: 50
-// }
-// };
-//
-//     let man1FullCopy // your code
-//
-//
-//     // 4. Array of primitives inside an object
-//     let man2 = {
-//     name: 'John',
-//     age: 28,
-//     friends: ["Peter", "Steven", "William"]
-// };
-//
-//     let man2FullCopy  // your code
-//
-//
-//     // 5 Array of objects
-//     let people = [
-//     {name: "Peter", age: 30},
-//     {name: "Steven", age: 32},
-//     {name: "William", age: 28}
-//     ];
-//
-//
-//     let peopleFullCopy  // your code
-//
-//
-//     // 6 Array of objects inside object
-//     let man3 = {
-//     name: 'John',
-//     age: 28,
-//     friends: [
-// {name: "Peter", age: 30},
-// {name: "Steven", age: 32},
-// {name: "William", age: 28}
-//     ]
-// };
-//
-//     let man3FullCopy //  your code
-//
-//
-//     // 7 Object inside an object, inside an object
-//     let man4 = {
-//     name: 'John',
-//     age: 28,
-//     mother: {
-//     name: "Kate",
-//     age: 50,
-//     work: {
-//     position: "doctor",
-//     experience: 15
-// }
-// }
-// };
-//
-//     let man4FullCopy //  your code
-//
-//     // 8 Array of objects inside object -> object
-//     let man5 = {
-//     name: 'John',
-//     age: 28,
-//     mother: {
-//     name: "Kate",
-//     age: 50,
-//     work: {
-//     position: "doctor",
-//     experience: 15
-// },
-//     parents: [
-// {name: "Kevin", age: 80},
-// {name: "Jennifer", age: 78},
-//     ]
-// }
-// };
-//
-//     let man5FullCopy //  your code
-//
-//
-//     // 9 Object inside an object -> array -> object ->  object
-//     let man6 = {
-//     name: 'John',
-//     age: 28,
-//     mother: {
-//     name: "Kate",
-//     age: 50,
-//     work: {
-//     position: "doctor",
-//     experience: 15
-// },
-//     parents: [
-// {
-//     name: "Kevin",
-//     age: 80,
-//     favoriteDish: {
-//     title: "borscht"
-// }
-// },
-// {
-//     name: "Jennifer",
-//     age: 78,
-//     favoriteDish: {
-//     title: "sushi"
-// }
-// },
-//     ]
-// }
-// };
-//
-//     let man6FullCopy  //  your code
-//
-//
-//     //10 Array of objects inside an object -> object -> array -> object ->  object
-//     let man7 = {
-//     name: 'John',
-//     age: 28,
-//     mother: {
-//     name: "Kate",
-//     age: 50,
-//     work: {
-//     position: "doctor",
-//     experience: 15
-// },
-//     parents: [
-// {
-//     name: "Kevin",
-//     age: 80,
-//     favoriteDish: {
-//     title: "borscht",
-//     ingredients: [
-// {title: "beet", amount: 3},
-// {title: "potatoes", amount: 5},
-// {title: "carrot", amount: 1},
-//     ]
-// }
-// },
-// {
-//     name: "Jennifer",
-//     age: 78,
-//     favoriteDish: {
-//     title: "sushi",
-//     ingredients: [
-// {title: "fish", amount: 1},
-// {title: "rise", amount: 0.5}
-//     ]
-// }
-// },
-//     ]
-// }
-// };
-//
-//     let man7FullCopy  //  your code
-//
-// </script>
+// links to watch theory
+// https://www.youtube.com/watch?v=6napu-MGQDo&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=47
+// https://www.youtube.com/watch?v=I8LNJpG60vI&feature=youtu.be
+
+// 1. Simple object
+let man = {
+    name: 'John',
+    age: 28
+};
+
+let manFullCopy = {...man}
+man.age = 36
+manFullCopy.name = 'Dima'
+console.log(man)
+console.log(manFullCopy)
+
+
+
+// 2. Array of primitives
+let numbers = [1, 2, 3];
+
+let numbersFullCopy  //  your code
+
+
+// 3. Object inside an object
+let man1 = {
+    name: 'John',
+    age: 28,
+    mother: {
+        name: 'Kate',
+        age: 50
+    }
+};
+
+let man1FullCopy // your code
+
+
+// 4. Array of primitives inside an object
+let man2 = {
+    name: 'John',
+    age: 28,
+    friends: ["Peter", "Steven", "William"]
+};
+
+let man2FullCopy  // your code
+
+
+// 5 Array of objects
+let people = [
+    {name: "Peter", age: 30},
+    {name: "Steven", age: 32},
+    {name: "William", age: 28}
+];
+
+
+let peopleFullCopy  // your code
+
+
+// 6 Array of objects inside object
+let man3 = {
+    name: 'John',
+    age: 28,
+    friends: [
+        {name: "Peter", age: 30},
+        {name: "Steven", age: 32},
+        {name: "William", age: 28}
+    ]
+};
+
+let man3FullCopy //  your code
+
+
+// 7 Object inside an object, inside an object
+let man4 = {
+    name: 'John',
+    age: 28,
+    mother: {
+        name: "Kate",
+        age: 50,
+        work: {
+            position: "doctor",
+            experience: 15
+        }
+    }
+};
+
+let man4FullCopy //  your code
+
+// 8 Array of objects inside object -> object
+let man5 = {
+    name: 'John',
+    age: 28,
+    mother: {
+        name: "Kate",
+        age: 50,
+        work: {
+            position: "doctor",
+            experience: 15
+        },
+        parents: [
+            {name: "Kevin", age: 80},
+            {name: "Jennifer", age: 78},
+        ]
+    }
+};
+
+let man5FullCopy //  your code
+
+
+// 9 Object inside an object -> array -> object ->  object
+let man6 = {
+    name: 'John',
+    age: 28,
+    mother: {
+        name: "Kate",
+        age: 50,
+        work: {
+            position: "doctor",
+            experience: 15
+        },
+        parents: [
+            {
+                name: "Kevin",
+                age: 80,
+                favoriteDish: {
+                    title: "borscht"
+                }
+            },
+            {
+                name: "Jennifer",
+                age: 78,
+                favoriteDish: {
+                    title: "sushi"
+                }
+            },
+        ]
+    }
+};
+
+let man6FullCopy  //  your code
+
+
+//10 Array of objects inside an object -> object -> array -> object ->  object
+let man7 = {
+    name: 'John',
+    age: 28,
+    mother: {
+        name: "Kate",
+        age: 50,
+        work: {
+            position: "doctor",
+            experience: 15
+        },
+        parents: [
+            {
+                name: "Kevin",
+                age: 80,
+                favoriteDish: {
+                    title: "borscht",
+                    ingredients: [
+                        {title: "beet", amount: 3},
+                        {title: "potatoes", amount: 5},
+                        {title: "carrot", amount: 1},
+                    ]
+                }
+            },
+            {
+                name: "Jennifer",
+                age: 78,
+                favoriteDish: {
+                    title: "sushi",
+                    ingredients: [
+                        {title: "fish", amount: 1},
+                        {title: "rise", amount: 0.5}
+                    ]
+                }
+            },
+        ]
+    }
+};
+
+let man7FullCopy  //  your code
+
+
+
+
+
+
+
+
 
 
 //1 this method is sorting out of the box without additional parameters
@@ -399,11 +409,9 @@ let b = {...a}
 b.classroom = {...a.classroom}
 b.classroom.teacher = {...a.classroom.teacher}
 b.students = [...a.students]
-
 b.classroom.teacher.name = 'Dmitry'
 
-console.log(a.classroom.teacher.name)
-console.log(b.classroom.teacher.name)
+
 
 
 
