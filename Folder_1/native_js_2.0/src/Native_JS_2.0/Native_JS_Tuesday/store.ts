50186d944bbf83a607b5e9c6596f455b3d70c585
DmitryShastel <dmshastel@gmail.com>
2023-03-09 19:29:32 +0300

    The type for array1 is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/store.ts
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/store.ts
@@ -1,6 +1,8 @@
 type AgesType = Array<number>
+type AgesType2 = Array<number | string | boolean>
 
 let ages: AgesType = [12, 2, 5, 88]
+let ages2: AgesType2 = [12, 2, 5, 88, 'aa', true]
 
 
 export default 1
