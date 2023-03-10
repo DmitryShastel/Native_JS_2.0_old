6399b5db2f15bf0bacdfc0771e9a6e75c9ba81f0
DmitryShastel <dmshastel@gmail.com>
2023-03-10 19:28:52 +0300

    The type for array2 is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/store.ts
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/store.ts
@@ -5,4 +5,33 @@ let ages: AgesType = [12, 2, 5, 88]
 let ages2: AgesType2 = [12, 2, 5, 88, 'aa', true]
 
 
+// type ArraysType = {
+//     name?: string
+//     height?: number
+//     model?: string
+//     year?: number
+// }
+
+
+interface IMan {
+    name: string
+    height: number
+}
+
+interface ICar {
+    model: string
+    year: number
+}
+
+let man1: IMan = {name: 'Dima', height: 1.78}
+let man2: IMan = {name: 'Alex', height: 1.78}
+let car: ICar = {model: 'BMW', year: 2022}
+
+type IArr = Array<IMan>
+
+let arr: IArr = [
+    {name: 'Dima', height: 1.78},
+    {name: 'Alex', height: 1.78}
+]
+
 export default 1
