bb77ce89ebb1e66c7e81b44dca952823f4d68c8c
DmitryShastel <dmshastel@gmail.com>
2023-03-13 20:22:50 +0300

    The swich/case logic is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/Application-Redux/reducers/reducers.ts
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/Application-Redux/reducers/reducers.ts
@@ -8,6 +8,16 @@ const defaultState = {
     cash: 0
 }
 
-export const reducer = (state: any, action: any) => {
+export const reducer = (state: DefaultStateType = defaultState, action: any) => {
+    switch (action.type) {
 
+        case 'ADD_MONEY':
+            return {...state, cash: state.cash + action.payload}
+
+        case 'GET_MONEY':
+            return {...state, cash: state.cash - action.payload}
+        
+        default:
+            return state
+    }
 }
