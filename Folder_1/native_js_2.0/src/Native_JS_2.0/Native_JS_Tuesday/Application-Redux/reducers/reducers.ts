6e7e9c21bf6ed4a51891963ef77ed4747a7b86e2
DmitryShastel <dmshastel@gmail.com>
2023-03-13 20:36:51 +0300

    The ActionType is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/Application-Redux/reducers/reducers.ts
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/Application-Redux/reducers/reducers.ts
@@ -1,14 +1,17 @@
-import React from 'react';
-
 type DefaultStateType = {
     cash: number
+}
+
+type ActionType = {
+    type: string
+    payload: number
 }
 
 const defaultState = {
     cash: 0
 }
 
-export const reducer = (state: DefaultStateType = defaultState, action: any) => {
+export const reducer = (state: DefaultStateType = defaultState, action: ActionType) => {
     switch (action.type) {
 
         case 'ADD_MONEY':
@@ -16,7 +19,7 @@ export const reducer = (state: DefaultStateType = defaultState, action: any) => 
 
         case 'GET_MONEY':
             return {...state, cash: state.cash - action.payload}
-        
+
         default:
             return state
     }
