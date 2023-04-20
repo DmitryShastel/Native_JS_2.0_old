842a1d2d7c00b11d346b367b1f90ae068707e563
unknown <dmshastel@gmail.com>
2023-04-20 18:31:41 +0300

    The NJ AddItemForm body is added

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/ToDoList_6/AddItemForm.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/ToDoList_6/AddItemForm.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const AddItemForm = () => {
+
+
+
+
+export const AddItemForm = () => {
+
+    let [title, setTitle] = useState(" ")
+    let [error, setError] = useState<string | null>(null)
+
+
     return (
         <div>
-            
+            <h3>{props.title}
+                <button onClick={removeTodolist}>x</button>
+            </h3>
+            <div>
+                <input value={title}
+                       onChange={onChangeHandler}
+                       onKeyPress={onKeyPressHandler}/>
+                <button onClick={addTask}>+
+                </button>
+                {error && <div className={"error-message"}>{error}</div>}
+            </div>
         </div>
-    );
-};
-
-export default AddItemForm;
+    )
+}
