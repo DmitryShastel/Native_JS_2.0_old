70e224f15cd1a2f66954b5cc686c54c859e9e0fd
unknown <dmshastel@gmail.com>
2023-04-24 09:13:59 +0300

    onDoubleClickHandler is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/ToDoList_6/EditableSpan.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/ToDoList_6/EditableSpan.tsx
@@ -11,9 +11,13 @@ export const EditableSpan = (props: EditableSpanType) => {
     const [updateTitle, setUpdateTitle] = useState(props.oldTitle)
     const [edit, setEdit] = useState(false)
 
+    const onDoubleClickHandler = () => {
+        setEdit(true)
+    }
+
     return (
         edit
-            ? <input type='text'/>
-            : <span>{props.oldTitle}</span>
+            ? <input type='text' value={updateTitle}/>
+            : <span onDoubleClick={onDoubleClickHandler}>{props.oldTitle}</span>
     );
 };
