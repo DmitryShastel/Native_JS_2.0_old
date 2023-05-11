9734b7441ba596a4b426616fdefbbccc4c937907
Dmitry Shastel <dmshastel@gmail.com>
2023-05-11 18:15:04 +0300

    Todolist#9 changeFilterACType is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/ToDolist_9/State/todolists-reducer.test.ts
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/ToDolist_9/State/todolists-reducer.test.ts
@@ -1,6 +1,7 @@
 import {v1} from "uuid";
-import {TodolistType} from "../AppRoot_9";
-import {TodolistsReducer, addTodolistAC, removeTodolistAC, changeTodolistAC} from "./todolists-reducer";
+import {TodolistType } from "../AppRoot_9";
+import {FilterValuesType } from "../AppRoot_9";
+import {TodolistsReducer, addTodolistAC, removeTodolistAC, changeTodolistAC, changeFilterAC} from "./todolists-reducer";
 
 
 test('correct todolist should be removed', () => {
@@ -57,4 +58,23 @@ test('correct todolist name should be changed', () => {
     expect(endState.length).toBe(2);
     expect(endState[0].title).toBe('What to learn');   
     expect(endState[1].title).toBe(newTodolistTitle); 
+}) ;
+
+test('correct filter of todolist should be changed', () => {
+    let todolistId1 = v1();
+    let todolistId2 = v1();
+
+    let newFilter: FilterValuesType = 'completed'
+
+    const startState: Array<TodolistType> = [
+        {id: todolistId1, title: "What to learn", filter: "all"},
+        {id: todolistId2, title: "What to buy", filter: "all"}
+    ]
+    
+    const endState = TodolistsReducer(startState,  changeTodolistAC(todolistId2, newFilter))
+
+
+    expect(endState.length).toBe(2);
+    expect(endState[0].filter).toBe('all');   
+    expect(endState[1].filter).toBe(newFilter); 
 }) ;
