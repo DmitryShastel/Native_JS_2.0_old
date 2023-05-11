5f46ff58d9b5e2a52d7657b9a4bbc6b072d1e81e
Dmitry Shastel <dmshastel@gmail.com>
2023-05-11 16:06:14 +0300

    Todolist#9 removeTodolistAC is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/ToDolist_9/State/todolists-reducer.ts
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/ToDolist_9/State/todolists-reducer.ts
@@ -1,12 +1,22 @@
-import React from 'react';
-import {TodolistType} from "../AppRoot_9";
+import React from "react";
+import { TodolistType } from "../AppRoot_9";
 
-export const TodolistsReducer = (state: TodolistType[], action: any): TodolistType[] => {
-    switch (action.type) {
-        case 'REMOVE-TODOLIST': {
-            return  state.filter(t => t.id != action.id)
-        }
-        default:
-            return state
+export const TodolistsReducer = (
+  state: TodolistType[],
+  action: any
+): TodolistType[] => {
+  switch (action.type) {
+    case "REMOVE-TODOLIST": {
+      return state.filter((t) => t.id != action.id);
     }
-}
+    default:
+      return state;
+  }
+};
+
+export const removeTodolistAC = (todolistID: string) => {
+  return {
+    action: "REMOVE-TODOLIST",
+    id: todolistID,
+  };
+};
