6bfbdf1e1d50052e73c09afd28d5203712a39a2a
Dmitry Shastel <dmshastel@gmail.com>
2023-05-13 08:54:02 +0300

    Todolist#9 PropsType is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/ToDolist_9/SuperCheckbox.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/ToDolist_9/SuperCheckbox.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+type PropsType = {
+  callBack: () => void;
+  isDone: boolean;
+};
 
-export const SuperCheckbox = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+export const SuperCheckbox = (props: PropsType) => {
+  let onChangeHandler = () => {
+    props.callBack();
+  };
 
-
+  return <input checked={props.isDone} onChange={onChangeHandler} />;
+};
