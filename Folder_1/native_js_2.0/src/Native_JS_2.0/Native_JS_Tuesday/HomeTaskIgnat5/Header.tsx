254c88c555c09ae7866c257d3a7a438b288ade6a
Dmitry Shastel <dmshastel@gmail.com>
2023-07-07 10:45:13 +0300

    The handleDrawerClose is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat5/Header.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat5/Header.tsx
@@ -4,10 +4,16 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import MenuIcon from '@material-ui/icons/Menu';
+import {Drawer} from "@mui/material";
 
 export const Header = () => {
 
+    // Состояние для открытия/закрытия бокового меню
     const [open, setOpen] = useState(false)
+
+    const handleDrawerClose = () => {
+        setOpen(false)
+    }
 
     const styles = {
         iconButton: {
@@ -20,13 +26,17 @@ export const Header = () => {
     return (
         <div>
             <AppBar position="static" color="default" elevation={0}>
-                <Toolbar >
-                    <IconButton edge="start" color="inherit"  aria-label="menu" style={styles.iconButton}>
-                        <MenuIcon />
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu" style={styles.iconButton}>
+                        <MenuIcon/>
                     </IconButton>
                     <Typography variant="h6" color="black">Pre-junior</Typography>
                 </Toolbar>
             </AppBar>
+
+            <Drawer anchor='left' open={open} onClose={handleDrawerClose}>
+            </Drawer>
+
         </div>
     );
 };
