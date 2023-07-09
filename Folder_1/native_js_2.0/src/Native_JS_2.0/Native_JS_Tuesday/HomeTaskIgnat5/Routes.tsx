347ec3c19ca42baee70fbaa6131decdacadc7149
unknown <dmshastel@gmail.com>
2023-07-09 09:18:25 +0300

    The Home_Tasks_2.0 location is added

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat5/Routes.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat5/Routes.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 import {Junior} from "./Pages/Junior";
 import {JuniorPlus} from "./Pages/JuniorPlus";
 import {Error404} from "./Pages/Error404";
 import {PreJunior} from "./Pages/PreJunior";
-
+import {HomePage} from "./Pages/HomePage";
 
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
     JUNIOR: '/junior',
     JUNIOR_PLUS: '/junior-plus',
+    HOME_PAGE: '/localhost:3000',
 }
 
 
 export const NavbarRoutes = () => {
+
+    const location = useLocation()
+
     return (
         <Routes>
             <Route path={PATH.JUNIOR} element={<Junior/>}/>
             <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
             <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
-            <Route path="*" element={<Error404/>}/>
+            <Route path={PATH.HOME_PAGE} element={<HomePage/>}/>
+            <Route path="*" element={PATH.HOME_PAGE !== location.pathname &&  <Error404/>}/>
         </Routes>
     );
 };
