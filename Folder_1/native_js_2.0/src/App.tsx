d62c35fbf7180fcb020846d5174429acb21eccd9
Dmitry Shastel <dmshastel@gmail.com>
2023-07-24 10:03:08 +0300

    Home Task #7 is created

--- Folder_1/native_js_2.0/src/App.tsx
+++ Folder_1/native_js_2.0/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {HW6} from "./Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat6/HW6";
 import  './App.css'
+import { HW7 } from './Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat7/HW7';
 
 export function App() {
     return (
         <div>
-            <HW6/>
+            <HW7/>
         </div>
 
     );
---
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat7/HW7.tsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+import {SuperSelect} from './SuperSelect'
+import {SuperRadio} from './SuperRadio'
+
+const arr = ['x', 'y', 'z']
+
+export function HW7() {
+    const [value, onChangeOption] = useState(arr[1])
+
+    return (
+        <div>
+            <hr/>
+            homeworks 7
+
+            {/*should work (должно работать)*/}
+            <div>
+                <SuperSelect
+                    options={arr}
+                    value={value}
+                    onChangeOption={onChangeOption}
+                />
+            </div>
+            <div>
+                <SuperRadio
+                    name={'radio'}
+                    options={arr}
+                    value={value}
+                    onChangeOption={onChangeOption}
+                />
+            </div>
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeSuperSelect/>*/}
+            {/*<AlternativeSuperRadio/>*/}
+            <hr/>
+        </div>
+    )
+}
