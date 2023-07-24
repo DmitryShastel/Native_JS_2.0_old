579e7741ecc7c7b29353967f09b74836491cba3b
Dmitry Shastel <dmshastel@gmail.com>
2023-07-24 11:58:53 +0300

    Home TaskT #7 fun handleChange is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat7/TestHW7/HW7T.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat7/TestHW7/HW7T.tsx
@@ -1,9 +1,18 @@
-import React, {useState} from 'react'
+import React, {useState, ChangeEvent} from 'react'
 import s from "./SuperHW7T.module.css"
 import {SuperRadioT} from './SuperRadioT'
 
 
+
 export function HW7T() {
+
+const [selectedOption, setSelectedOption] = useState('')
+
+
+let handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSelectedOption(e.currentTarget.value)
+}
+
 
     return (
         <div className={s.parent}>
--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat7/TestHW7/SuperRadioT.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat7/TestHW7/SuperRadioT.tsx
@@ -5,20 +5,20 @@ import React, {
 } from "react";
 
 export const SuperRadioT = () => {
-  return <div>
-
-        <label>
-            <input type='radio'  value='option1'/>
-            Option 1
-        </label>
-        <label>
-            <input type='radio'  value='option2'/>
-            Option 2
-        </label>
-        <label>
-            <input type='radio'  value='option3'/>
-            Option 3
-        </label>
-
-  </div>;
+  return (
+    <div>
+      <label>
+        <input type="radio" value="option1" />
+        Option 1
+      </label>
+      <label>
+        <input type="radio" value="option2" />
+        Option 2
+      </label>
+      <label>
+        <input type="radio" value="option3" />
+        Option 3
+      </label>
+    </div>
+  );
 };
