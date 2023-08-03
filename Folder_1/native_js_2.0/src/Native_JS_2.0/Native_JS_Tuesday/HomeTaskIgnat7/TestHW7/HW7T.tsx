3ad293f548b7ce1045c9c3e0da0b9e80b607b4c8
unknown <dmshastel@gmail.com>
2023-08-03 12:40:56 +0300

    Home SuperRadioT is changed

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat7/TestHW7/HW7T.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat7/TestHW7/HW7T.tsx
@@ -1,31 +1,30 @@
-import React, { useState, ChangeEvent } from "react";
+import React, {useState, ChangeEvent} from "react";
 import s from "./SuperHW7T.module.css";
-import { SuperRadioT } from "./SuperRadioT";
-import { SuperSelect } from "./SuperSelectT";
+import {SuperRadioT} from "./SuperRadioT";
+import {SuperSelectT} from "./SuperSelectT";
 
- const options = [1, 2, 3];
+const options = [1, 2, 3];
 
 export function HW7T() {
-  const [selectedOption, setSelectedOption] = useState("");
-  //const [value, setValue] = useState("");
+    const [selectedOption, setSelectedOption] = useState("");
 
-  let handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSelectedOption(e.currentTarget.value);
-    // setValue(e.currentTarget.value);
-  };
+    let handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setSelectedOption(e.currentTarget.value);
+    };
 
-  return (
-    <div className={s.parent}>
-      <SuperRadioT
-        options={options}
-        setSelectedOption={selectedOption}
-        handleChange={handleChange}
-      />
-      <SuperSelect
-        value={selectedOption}
-        handleChange={handleChange}
-        options={options}
-      />
-    </div>
-  );
+    return (
+        <div className={s.parent}>
+            <SuperRadioT
+                options={options}
+                value={selectedOption}
+                handleChange={handleChange}
+            />
+            <SuperSelectT
+                options={options}
+                value={selectedOption}
+                handleChange={handleChange}
+
+            />
+        </div>
+    );
 }
--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat7/TestHW7/SuperRadioT.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat7/TestHW7/SuperRadioT.tsx
@@ -1,76 +1,57 @@
-import React, {
-  ChangeEvent,
-  InputHTMLAttributes,
-  DetailedHTMLProps,
-} from "react";
+import React, {ChangeEvent,} from "react";
 
 
 type SuperRadioTType = {
-  setSelectedOption: string;
-  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  options:any [] 
+    value: string;
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    options: any []
 };
 
 export const SuperRadioT = (props: SuperRadioTType) => {
-  // const radioOptions = [ 1, 2, 3];
 
-  let mappedRadioOptions = props.options.map((option) => (
-    <label key={option} >
-      <input
-        type="radio"
-        value={option}
-        checked={props.setSelectedOption === option.toString()}
-        onChange={props.handleChange}
-      />
-      {option}
-    </label>
-  ));
+    let mappedRadioOptions = props.options.map((option) => (
+        <label key={option}>
+            <input
+                type="radio"
+                value={option}
+                checked={props.value === option}
+                onChange={props.handleChange}
+            />
+            {option}
+        </label>
+    ));
 
-  return <div>{mappedRadioOptions}</div>;
+    return <div>{mappedRadioOptions}</div>;
 };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 {
-  /* <label>
-        <input
-          type="radio"
-          value="option1"
-          checked={props.setSelectedOption === "option1"}
-          onChange={props.handleChange}
-        />
-      </label>
-      <label>
-        <input
-          type="radio"
-          value="option2"
-          checked={props.setSelectedOption === "option2"}
-          onChange={props.handleChange}
-        />
-        Option 2
-      </label>
-      <label>
-        <input
-          type="radio"
-          value="option3"
-          checked={props.setSelectedOption === "option3"}
-          onChange={props.handleChange}
-        />
-        Option 3
-      </label> */
+    /* <label>
+          <input
+            type="radio"
+            value="option1"
+            checked={props.setSelectedOption === "option1"}
+            onChange={props.handleChange}
+          />
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="option2"
+            checked={props.setSelectedOption === "option2"}
+            onChange={props.handleChange}
+          />
+          Option 2
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="option3"
+            checked={props.setSelectedOption === "option3"}
+            onChange={props.handleChange}
+          />
+          Option 3
+        </label> */
 }
 
 
@@ -87,5 +68,5 @@ export const SuperRadioT = (props: SuperRadioTType) => {
 // ));
 
 // { label: 1, value: "option1" },
-    // { label: 2, value: "option2" },
-    // { label: 3, value: "option3" },
+// { label: 2, value: "option2" },
+// { label: 3, value: "option3" },
