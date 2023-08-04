9132b9e1a72efa5a24e1c1e6e6445d188053277f
unknown <dmshastel@gmail.com>
2023-08-04 19:24:32 +0300

    HomeTask#8 all reducers are created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat8/homeWorkReducer.test.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat8/homeWorkReducer.test.tsx
@@ -15,40 +15,44 @@ let initialState: StateType[];
 
 beforeEach(() => {
     initialState = [
-        {_id: 0, name: "Кот", age: 3},
-        {_id: 1, name: "Александр", age: 66},
-        {_id: 2, name: "Коля", age: 16},
-        {_id: 3, name: "Виктор", age: 44},
-        {_id: 4, name: "Дмитрий", age: 40},
-        {_id: 5, name: "Ирина", age: 55},
+        {_id: 0, name: "Cot", age: 3},
+        {_id: 1, name: "Alex", age: 66},
+        {_id: 2, name: "Kolya", age: 16},
+        {_id: 3, name: "Victor", age: 44},
+        {_id: 4, name: "Dima", age: 40},
+        {_id: 5, name: "Ira", age: 55},
     ]
 });
 
 test("sort name up", () => {
-    const newState = homeWorkReducer(initialState, {type: "sort", payload: "up"});
+    const newState = homeWorkReducer(initialState, {type: "sort-up", payload: "up"});
 
     console.log(newState);
-    expect(newState[0].name).toBe('Александр');
-    expect(newState[1].name).toEqual('Виктор');
-    expect(newState[2].name).toEqual('Дмитрий');
-    expect(newState[3].name).toEqual('Ирина');
-    expect(newState[4].name).toEqual('Кот');
-    expect(newState[5].name).toEqual('Коля');
+    expect(newState[0].name).toEqual('Alex');
+    expect(newState[1].name).toEqual('Cot');
+    expect(newState[2].name).toEqual('Dima');
+    expect(newState[3].name).toEqual('Ira');
+    expect(newState[4].name).toEqual('Kolya');
+    expect(newState[5].name).toEqual('Victor');
 });
 test("sort name down", () => {
-    const newState = homeWorkReducer(initialState, {type: "sort", payload: "down"});
-    expect(newState[0].name).toBe('Кот');
-    expect(newState[1].name).toBe('Коля');
-    expect(newState[2].name).toBe('Ирина');
-    expect(newState[3].name).toBe('Дмитрий');
-    expect(newState[4].name).toBe('Виктор');
-    expect(newState[5].name).toBe('Александр');
+    const newState = homeWorkReducer(initialState, {type: "sort-down", payload: "down"});
+    expect(newState[0].name).toBe('Victor');
+    expect(newState[1].name).toBe('Kolya');
+    expect(newState[2].name).toBe('Ira');
+    expect(newState[3].name).toBe('Dima');
+    expect(newState[4].name).toBe('Cot');
+    expect(newState[5].name).toBe('Alex');
+
+    console.log(newState)
 });
 test("check age 18", () => {
     const newState = homeWorkReducer(initialState, {type: "check", payload: 18});
 
     expect(newState[0].age).toBe(66);
-    expect(newState[1].age).toBe(44);
-    expect(newState[2].age).toBe(40);
-    expect(newState[3].age).toBe(55);
+    expect(newState[1].age).toBe(55);
+    expect(newState[2].age).toBe(44);
+    expect(newState[3].age).toBe(40);
+
+    console.log(newState)
 });
--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat8/homeWorkReducer.ts
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat8/homeWorkReducer.ts
@@ -1,15 +1,29 @@
 import {ActionType, StateType} from "./homeWorkReducer.test";
 
+
+const SORT_UP = 'sort-up'
+const SORT_DOWN = 'sort-down'
+const CHECK = 'check'
+
+
 export const homeWorkReducer = (state: StateType[], action: ActionType): StateType[] => {
     switch (action.type) {
-        case "sort": {
-
-            return state
+        case SORT_UP: {
+            let copyState = [...state]
+            let result = copyState.slice().sort((a, b) => a.name.localeCompare(b.name));
+            return result
         }
-        case "check": {
-
-            return state
+        case SORT_DOWN: {
+            let copyState = [...state]
+            let result = copyState.slice().sort((a, b) => b.name.localeCompare(a.name));
+            return result
         }
-        default: return state
+        case CHECK: {
+            let copyState = [...state]
+            let result = copyState.filter((item) => item.age >= 18).sort((a, b) => b.age - a.age)
+            return result
+        }
+        default:
+            return state
     }
 };
