a6636f404e0fe2d264b352c92ea9a09af55edd7b
unknown <dmshastel@gmail.com>
2023-08-04 17:46:38 +0300

    HomeTask#8 is added

---
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/HomeTaskIgnat8/HW8.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import {homeWorkReducer} from "./homeWorkReducer";
+import {SuperButton} from "../HomeTaskIgnat6/common/SupperButton";
+
+
+const initialPeople = [
+    {_id: 0, name: "Кот", age: 3},
+    {_id: 1, name: "Александр", age: 66},
+    {_id: 2, name: "Коля", age: 16},
+    {_id: 3, name: "Виктор", age: 44},
+    {_id: 4, name: "Дмитрий", age: 40},
+    {_id: 5, name: "Ирина", age: 55},
+]
+
+export function HW8() {
+    const [people, setPeople] = useState(initialPeople);
+
+    const finalPeople = people.map(p => (
+        <div key={p._id}>
+            some name, age
+        </div>
+    ))
+
+    const sortUp = () => setPeople(homeWorkReducer(initialPeople, {type: "sort", payload: "up"}))
+
+    return (
+        <div>
+
+            {finalPeople}
+            <div><SuperButton onClick={sortUp}>sort up</SuperButton></div>
+            <div>sort down</div>
+
+            check 18
+            
+        </div>
+    );
+}
