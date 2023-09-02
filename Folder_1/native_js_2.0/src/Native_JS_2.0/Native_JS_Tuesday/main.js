54c45f79982d63676d438a68ccdbdf9789760834
unknown <dmshastel@gmail.com>
2023-09-02 20:42:07 +0300

    Task#4 split is added

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/main.js
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/main.js
@@ -825,23 +825,30 @@
 // console.log(newVegetables)
 
 //Colon between odd numbers
-const input = prompt('Введите число');
-let num = parseInt(input)
+// const input = prompt('Введите число');
+// let num = parseInt(input)
+//
+// let colonOdd = (num) => {
+//
+//     let numStr = num.toString()
+//     let result = ''
+//
+//     for (let i = 0; i < numStr.length; i++) {
+//         result += numStr[i]
+//
+//         if (parseInt(numStr[i]) % 2 !== 0 && parseInt(numStr[i + 1]) % 2 !== 0 && i !== numStr.length - 1){
+//             result += ':'
+//         }
+//     }
+//     return result
+// }
+//
+// let result = colonOdd(num)
+// console.log(result)
 
-let colonOdd = (num) => {
-
-    let numStr = num.toString()
-    let result = ''
-
-    for (let i = 0; i < numStr.length; i++) {
-        result += numStr[i]
-
-        if (parseInt(numStr[i]) % 2 !== 0 && parseInt(numStr[i + 1]) % 2 !== 0 && i !== numStr.length - 1){
-            result += ':'
-        }
-    }
-    return result
+let changeRegister = (str) => {
+    let newStr = str.split([,])
+    return newStr
 }
 
-let result = colonOdd(num)
-console.log(result)
+console.log(changeRegister('qaz'))
