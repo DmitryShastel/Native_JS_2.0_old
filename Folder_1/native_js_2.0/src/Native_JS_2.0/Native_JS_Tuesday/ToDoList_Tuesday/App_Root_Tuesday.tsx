67bfcb917a8c184e80c377c5d061eb98d5bddcc7
unknown <dmshastel@gmail.com>
2023-10-01 22:27:50 +0300

    ToDoListTuesday: todolists is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/ToDoList_Tuesday/App_Root_Tuesday.tsx
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Tuesday/ToDoList_Tuesday/App_Root_Tuesday.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {TodoListTuesday} from "./TodoList_Tuesday";
+import {v1} from "uuid";
 
 export const AppRootTuesday = () => {
+
+    let todolistId1 = v1()
+    let todolistId2 = v1()
+
+    let [todolists, setTodolists] = useState([
+        {id: todolistId1, title: 'What to learn', filter: 'all'},
+        {id: todolistId2, title: 'What to buy', filter: 'all'},
+    ])
+
+
     return (
         <div>
             <TodoListTuesday/>
