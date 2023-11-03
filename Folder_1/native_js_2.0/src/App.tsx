9d6e233c62ba1e455ae5bbb94f50f71c03c8dffd
unknown <dmshastel@gmail.com>
2023-11-03 18:07:50 +0300

    Counter_2.0: Counter is added

--- Folder_1/native_js_2.0/src/App.tsx
+++ Folder_1/native_js_2.0/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css'
 import {AppRootTuesday} from "./Native_JS_2.0/Native_JS_Tuesday/ToDoList_Tuesday/App_Root_Tuesday";
+import {Counter} from "./Native_JS_2.0/Native_JS_Monday/Counter/Counter";
 
 
 export function App() {
     return (
         <div>
-            <AppRootTuesday/>
+
         </div>
 
     );
