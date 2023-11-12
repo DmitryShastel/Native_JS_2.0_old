978084d246c39dc43f3664d29fe21ec77faccc97
unknown <dmshastel@gmail.com>
2023-11-12 20:11:20 +0300

    js_wednesday: example 2 is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Wednesday/native_js/Lesson_1/js_9.js
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Wednesday/native_js/Lesson_1/js_9.js
@@ -1,1 +1,20 @@
-console.log('hello')
+// let car = 'bmw'
+//
+// let startEngine = () => {
+//     console.log(`Start ${car}`)
+// }
+//
+// car = 'audi'
+//
+// startEngine()
+
+const a = 10;
+const b = 20;
+
+function foo() {
+    const a = 30;
+    const b = 40;
+    console.log(a, b)
+}
+
+foo()
