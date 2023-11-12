70da03e0a7404e4dea3a0d7238d9b3297434d269
unknown <dmshastel@gmail.com>
2023-11-12 20:28:17 +0300

    js_wednesday: example 4 is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Wednesday/native_js/Lesson_1/js_9.js
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Wednesday/native_js/Lesson_1/js_9.js
@@ -10,12 +10,17 @@
 
 const a = 10;
 const b = 20;
+const c = 50;
 
 function foo() {
     const a = 30;
     const b = 40;
     console.log(a, b)
     foo2();
+    const bar = () => {
+        console.log(c)
+    }
+    bar();
 }
 
 foo()
