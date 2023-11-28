31fa379acd15b9739f067fca2474725dba6dd032
unknown <dmshastel@gmail.com>
2023-11-28 13:18:59 +0300

    js_wednesday: Lesson 3 - chain of promise is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Wednesday/native_js/Lesson_3/js_11.js
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Wednesday/native_js/Lesson_3/js_11.js
@@ -2,12 +2,27 @@ const server = {
     getData() {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
-                //resolve();
-                reject();
+                // resolve('some data');
+                reject('some error');
             }, 2000)
         })
     }
 }
 
-const promise = server.getData();
-console.log(promise);
+// const promise = server.getData();
+// console.log(promise);
+
+server.getData()
+    .then((data) => {
+        console.log('then1', data);
+        return 10
+    })
+    .then((data) => {
+        console.log('then2', data);
+    })
+    .catch((error) => {
+        console.log('cath1', error)
+    })
+    .then((data) => {
+        console.log('then3', data);
+    })
