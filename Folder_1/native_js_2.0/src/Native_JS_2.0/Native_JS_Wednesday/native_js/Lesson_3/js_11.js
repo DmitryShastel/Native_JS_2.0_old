65e2bf1f55bccdf696512bd96821767d02b44673
unknown <dmshastel@gmail.com>
2023-12-04 15:34:43 +0300

    js_wednesday: Lesson 3 - async/await is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Wednesday/native_js/Lesson_3/js_11.js
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Wednesday/native_js/Lesson_3/js_11.js
@@ -38,32 +38,43 @@ const findUserInDB = (id) => {
 }
 
 
-const lastPromise = findUserInDB(1)
-    .then(user => {
-        console.log(user)
-        return user;
-    })
-    .then(user => findUserInDB(user.friend))
-    .then(friend1 => {
-        console.log(friend1)
-        return friend1
-    })
-    .then(friend1 => findUserInDB(friend1.friend))
-    .then(friend2 => console.log(friend2.name))
+async function run () {
+    let user = await findUserInDB(1)
+    console.log(user.name)
+    let friend1 = await findUserInDB(user.friend)
+    console.log(friend1.name)
+    let friend2 = await findUserInDB(friend1.friend)
+    console.log(friend2.name)
+}
+
+run();
 
 
-const promise1 = axios.get('test_url')
-    .then((data) => {
-        return data
-        // return  console.log(data)
-    })
+//CHAIN of promise
+// const lastPromise = findUserInDB(1)
+//     .then(user => {
+//         console.log(user)
+//         return user;
+//     })
+//     .then(user => findUserInDB(user.friend))
+//     .then(friend1 => {
+//         console.log(friend1)
+//         return friend1
+//     })
+//     .then(friend1 => findUserInDB(friend1.friend))
+//     .then(friend2 => console.log(friend2.name))
 
-const promise2 = findUserInDB(1)
-    .then((userData) => {
-        return userData
-    })
-    .catch(() => {
-    })
+// const promise1 = axios.get('test_url')
+//     .then((data) => {
+//         return data
+//         // return  console.log(data)
+//     })
+// const promise2 = findUserInDB(1)
+//     .then((userData) => {
+//         return userData
+//     })
+//     .catch(() => {
+//     })
 
 //ALL Promise
 //const otherPromise = Promise.all([promise1, promise2])
