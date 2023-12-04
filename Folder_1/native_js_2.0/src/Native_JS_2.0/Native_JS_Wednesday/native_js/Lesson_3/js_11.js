55162f24ed8bca37ad26539c7da1b72f1986b39d
unknown <dmshastel@gmail.com>
2023-12-04 15:07:57 +0300

    js_wednesday: Lesson 3 - lastPromise is created

--- Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Wednesday/native_js/Lesson_3/js_11.js
+++ Folder_1/native_js_2.0/src/Native_JS_2.0/Native_JS_Wednesday/native_js/Lesson_3/js_11.js
@@ -1,14 +1,3 @@
-const server = {
-    getData() {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                // resolve('some data');
-                reject('some error');
-            }, 2000)
-        })
-    }
-}
-
 const axios = {
     get(url) {
         return new Promise((resolve, reject) => {
@@ -33,8 +22,9 @@ const findUserInDB = (id) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             const users = [
-                {id: 1, name: 'Bob'},
-                {id: 2, name: 'Anna'},
+                {id: 1, name: 'Bob', friend: 2},
+                {id: 2, name: 'Anna', friend: 1},
+                {id: 3, name: 'Sveta', friend: null},
             ];
             const filteredUser = users.filter((el) => el.id === id);
             if (filteredUser.length > 0) {
@@ -46,6 +36,21 @@ const findUserInDB = (id) => {
         })
     })
 }
+
+
+const lastPromise = findUserInDB(1)
+    .then(user => {
+        console.log(user)
+        return user;
+    })
+    .then(user => findUserInDB(user.friend))
+    .then(friend1 => {
+        console.log(friend1)
+        return friend1
+    })
+    .then(friend1 => findUserInDB(friend1.friend))
+    .then(friend2 => console.log(friend2.name))
+
 
 const promise1 = axios.get('test_url')
     .then((data) => {
@@ -60,9 +65,8 @@ const promise2 = findUserInDB(1)
     .catch(() => {
     })
 
-
+//ALL Promise
 //const otherPromise = Promise.all([promise1, promise2])
-
 // otherPromise
 //     .then((results) => {
 //         const dataFromServer = results[0];
@@ -74,26 +78,25 @@ const promise2 = findUserInDB(1)
 //         console.log('initialization failed. Try later')
 //     })
 
-const otherPromise2 = Promise.allSettled([promise1, promise2])
+//ALLSettled Promise
+// const otherPromise2 = Promise.allSettled([promise1, promise2])
+// otherPromise2
+//     .then((results) => {
+//         console.log(results)
+//
+//         const dataFromServer =
+//             results[0].status === "fulfilled"
+//                 ? results[0].value
+//                 : results[0].vacancies;
+//         const userFromDB = results[1].status === "fulfilled"
+//             ? results[1].value
+//             : {name: results[1].reason};
+//         console.log(dataFromServer.vacancies[0].title + '; ' + userFromDB.name)
+//     })
 
-otherPromise2
-    .then((results) => {
-        console.log(results)
-
-        const dataFromServer =
-            results[0].status === "fulfilled"
-                ? results[0].value
-                : results[0].vacancies;
-        const userFromDB = results[1].status === "fulfilled"
-            ? results[1].value
-            : {name: results[1].reason};
-        console.log(dataFromServer.vacancies[0].title + '; ' + userFromDB.name)
-    })
-
-
+//TEST Promise
 // const promise = server.getData();
 // console.log(promise);
-
 // server.getData()
 //     .then((data) => {
 //         console.log('then1', data);
@@ -111,3 +114,15 @@ otherPromise2
 // .finally(() => {
 //     console.log('finally')
 // })
+
+//FIRST Promise
+// const server = {
+//     getData() {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 // resolve('some data');
+//                 reject('some error');
+//             }, 2000)
+//         })
+//     }
+// }
